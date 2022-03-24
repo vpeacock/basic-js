fcb8fee7b4fb9a66bb858a817f9c49964b6bf726
[Valentina] <[mariaiscus1@gmail.com]>
2022-03-24 09:33:59 +0600

    feat: add task solution for simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chainArr: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chainArr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    value === undefined
+      ? this.chainArr.push("( )")
+      : this.chainArr.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (
+      position === undefined ||
+      typeof position != "number" ||
+      position <= 0 ||
+      position > this.chainArr.length
+    ) {
+      this.chainArr = [];
+      throw new Error(`You can't remove incorrect link!`);
+    }
+
+    this.chainArr.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chainArr = this.chainArr.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let chainJoinArr = this.chainArr.join("~~");
+    this.chainArr = [];
+    return chainJoinArr;
   }
 };
 
