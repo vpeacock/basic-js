91b7d563022ed1183ccdc31b474ed07129cf7de7
[Valentina] <[mariaiscus1@gmail.com]>
2022-03-24 09:24:08 +0600

    feat: add task solution for carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,20 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function dateSample(sampleActivity) {
+  if (
+    typeof sampleActivity === 'string' &&
+    typeof Number(sampleActivity) === 'number' &&
+    Number(sampleActivity) > 0
+  ) {
+    let solution = Math.ceil(
+      Math.log(MODERN_ACTIVITY / Number(sampleActivity)) /
+        (0.693 / HALF_LIFE_PERIOD)
+    );
+    if (solution < 0) return false;
+    return solution;
+  }
+  return false;
 }
 
 module.exports = {
