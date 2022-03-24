a7f89e44828e280064e7dac93c3f3553471d6ac7
[Valentina] <[mariaiscus1@gmail.com]>
2022-03-24 09:36:24 +0600

    feat: add task solution for transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  const operations = [
+    '--discard-next',
+    '--discard-prev',
+    '--double-next',
+    '--double-prev',
+  ];
+  let newArr = arr.slice();
+  let addArr = [];
+  let newArrFilter = [];
+  let tmp = 0;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (newArr[i] === operations[2] && i != newArr.length - 1) {
+      tmp = newArr
+        .slice(i, i + 2)
+        .filter((elem) => !operations.includes(elem))[0];
+      if (tmp != undefined) {
+        newArr.splice(i + 1, 0, tmp);
+      }
+      newArrFilter = newArr.filter((elem) => !operations.includes(elem));
+    } else if (newArr[i] === operations[3] && i != 0) {
+      tmp = newArr
+        .slice(i - 1, i)
+        .filter((elem) => !operations.includes(elem))[0];
+      if (tmp != undefined) {
+        newArr.splice(i + 1, 0, tmp);
+      }
+      newArrFilter = newArr.filter((elem) => !operations.includes(elem));
+    } else if (newArr[i] === operations[0] && i != newArr.length - 1) {
+      newArr.splice(i + 1, 1);
+      newArrFilter = newArr.filter((elem) => !operations.includes(elem));
+    } else if (newArr[i] === operations[1] && i != 0) {
+      newArr.splice(i - 1, 1);
+      newArrFilter = newArr.filter((elem) => !operations.includes(elem));
+    }else{
+      newArrFilter = newArr.filter((elem) => !operations.includes(elem));
+    }
+  }
+  return newArrFilter;
 }
+
 
 module.exports = {
   transform
