85902ce37ab9fa913953005a2ce80375b0589145
[Valentina] <[mariaiscus1@gmail.com]>
2022-03-27 14:00:35 +0600

    feat: add task solution for mac-address task

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  const arrGroup = n.split('-');
+  const arrElem = arrGroup.join('').split('');
+
+  if (arrGroup.length !== 6) return false;
+
+  const keys = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+  ];
+
+  for (const char of arrElem) {
+    if (!keys.includes(char)) {
+        console.log(char)
+      return false;
+    }
+  }
+  return true;
 }
 module.exports = {
   isMAC48Address
