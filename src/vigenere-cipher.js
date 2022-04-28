e09242e72382553dc8e82f2c9169c8d7568c3d35
[Valentina] <[mariaiscus1@gmail.com]>
2022-04-28 19:48:29 +0600

    feat: add task solution for vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,16 +19,91 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class VigenereCipheringMachine {
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    let result = [];
+
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let messageAlpha = message.match(/[A-Z]/g).join('');
+    let countRepeat = Math.ceil(message.length / key.length);
+    let repeatKey = key.repeat(countRepeat).slice(0, messageAlpha.length);
+
+    for (let i = 0; i < messageAlpha.length; i++) {
+      let charCode =
+        (messageAlpha[i].charCodeAt(0) + repeatKey[i].charCodeAt(0)) % 26;
+      charCode += 'A'.charCodeAt(0);
+      result.push(String.fromCharCode(charCode));
+    }
+
+    let symbols = message.match(/[^a-z]/gi);
+
+    if (symbols) {
+        for (let i = 0; i < message.length; i++) {
+          if (symbols.includes(message[i])) {
+            result.splice(i, 0, message[i]);
+          }
+        }
+      }
+  
+
+    if (this.direct) {
+      result = result.join('');
+    } else {
+      result = result.reverse().join('');
+    }
+
+    return result;
+  }
+
+  decrypt(message, key) {
+    let result = [];
+
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let messageAlpha = message.match(/[A-Z]/g).join('');
+    let countRepeat = Math.ceil(message.length / key.length);
+    let repeatKey = key.repeat(countRepeat).slice(0, messageAlpha.length);
+
+    for (let i = 0; i < messageAlpha.length; i++) {
+      let charCode =
+        (messageAlpha.charCodeAt(i) - repeatKey.charCodeAt(i) + 26) % 26;
+      charCode += 'A'.charCodeAt(0);
+      result.push(String.fromCharCode(charCode));
+    }
+
+    let symbols = message.match(/[^a-z]/gi);
+
+    if (symbols) {
+      for (let i = 0; i < message.length; i++) {
+        if (symbols.includes(message[i])) {
+          result.splice(i, 0, message[i]);
+        }
+      }
+    }
+
+    if (this.direct) {
+      result = result.join('');
+    } else {
+      result = result.reverse().join('');
+    }
+
+    return result;
   }
 }
+
 
 module.exports = {
   VigenereCipheringMachine
